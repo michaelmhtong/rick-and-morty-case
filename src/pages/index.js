f18a87e2ed96e91c2c7f8d7561dc3bfd71741c83
michael07200 <mickmhtong@gmail.com>
2023-03-03 10:31:59 +0100

    init

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Rick and Morty Case</title>
+        <meta name="description" content="Assignment of Gracious" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Home</div>
+    </div>
+  );
+}
