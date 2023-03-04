3263ab14113ab1b3ece62be07c32095c2cbabbd3
michael07200 <mickmhtong@gmail.com>
2023-03-04 18:25:41 +0100

    fix: add redirect route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/location",
+        destination: "/location/page/1",
+        permanent: false,
+      },
+      {
+        source: "/character",
+        destination: "/character/page/1",
+        permanent: false,
+      },
+      {
+        source: "/episode",
+        destination: "/episode/page/1",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
